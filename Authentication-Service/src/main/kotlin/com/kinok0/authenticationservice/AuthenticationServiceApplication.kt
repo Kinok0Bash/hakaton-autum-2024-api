29766637fb51cec29fea5cae281313c42cd84b68
Kinok0 <truthpruthbg@gmail.com>
2024-10-26 01:04:37 +0300

    ЭНТИТИ ЗАВЕДИТЕСЬ УМОЛЯЮ!!!!!!!!!!!

--- Authentication-Service/src/main/kotlin/com/kinok0/authenticationservice/AuthenticationServiceApplication.kt
+++ Authentication-Service/src/main/kotlin/com/kinok0/authenticationservice/AuthenticationServiceApplication.kt
@@ -4,9 +4,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.autoconfigure.domain.EntityScan
 import org.springframework.boot.runApplication
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableJpaRepositories(basePackages = ["com.kinok0.authenticationservice.repository"])
 @EntityScan(basePackages = ["com.kinok0.migrationservice.entity"])
 class AuthenticationServiceApplication
 
