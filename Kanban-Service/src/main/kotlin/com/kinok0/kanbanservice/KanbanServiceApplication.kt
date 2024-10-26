35322186fed15a41cd957adbc0a840ef587dedeb
Kinok0 <truthpruthbg@gmail.com>
2024-10-27 02:03:20 +0300

    Закончена подготовка к dev деплою

--- Kanban-Service/src/main/kotlin/com/kinok0/kanbanservice/KanbanServiceApplication.kt
+++ Kanban-Service/src/main/kotlin/com/kinok0/kanbanservice/KanbanServiceApplication.kt
@@ -2,9 +2,11 @@ package com.kinok0.kanbanservice
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories
 
 @SpringBootApplication
+@EnableDiscoveryClient
 @EnableJpaRepositories(basePackages = ["com.kinok0.kanbanservice.repository"])
 class KanbanServiceApplication
 
